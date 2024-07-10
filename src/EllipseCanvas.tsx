27bd8466432a070921727f93e5f8ada1ab99419e
ayumiohno <ayumi0130ohno@gmail.com>
2024-07-10 20:48:49 +0900

    fixed control panel

--- src/EllipseCanvas.tsx
+++ src/EllipseCanvas.tsx
@@ -19,7 +19,7 @@ const EllipseCanvas: React.FC = () => {
     const [keyMode, setKeyMode] = useState<KeyMode>('move');
     const [startRecording, stopRecording, volume] = useVolumeLevel();
     const [volumes, setVolumes] = useState<number[]>([]);
-    const [xyForVoice, setXYForVoice] = useState<'X' | 'Y'>('X');
+    const [xyForVoice, setXYForVoice] = useState<'x' | 'y'>('x');
 
     const handleStageClick = (e: any) => {
         const clientX = e.target.getStage().getPointerPosition().x;
@@ -73,13 +73,13 @@ const EllipseCanvas: React.FC = () => {
                             if (ellipse.idx === showGuides) {
                                 switch (e.key) {
                                     case 'ArrowUp':
-                                        return { ...ellipse, rotation: ellipse.rotation + 5 };
+                                        return { ...ellipse, rotation: ellipse.rotation + 2 };
                                     case 'ArrowDown':
-                                        return { ...ellipse, rotation: ellipse.rotation - 5 };
+                                        return { ...ellipse, rotation: ellipse.rotation - 2 };
                                     case 'ArrowLeft':
-                                        return { ...ellipse, rotation: ellipse.rotation - 5 };
+                                        return { ...ellipse, rotation: ellipse.rotation - 2 };
                                     case 'ArrowRight':
-                                        return { ...ellipse, rotation: ellipse.rotation + 5 };
+                                        return { ...ellipse, rotation: ellipse.rotation + 2 };
                                 }
                             }
                             return ellipse;
@@ -126,11 +126,11 @@ const EllipseCanvas: React.FC = () => {
         if (transcript.match(/[0-9]/) != null) {
             setShowGuides(parseInt(transcript.match(/[0-9]/)![0]));
             resetTranscript();
-        } else if (transcript.includes('M') || transcript.includes('S') || transcript.includes('R')) {
+        } else if (transcript.includes('M') || transcript.includes('S') || transcript.includes('R') || transcript.includes('あーる') || transcript.includes('ある')) {
             switch (transcript) {
                 case 'M': setKeyMode('move'); break;
                 case 'S': setKeyMode('scale'); break;
-                case 'R': setKeyMode('rotate'); break;
+                default: setKeyMode('rotate'); break;
             }
             resetTranscript();
         } else if (transcript.includes('A')) {
@@ -144,10 +144,10 @@ const EllipseCanvas: React.FC = () => {
             setEllipseNumber(prev => prev + 1);
             resetTranscript();
         } else if (transcript.includes('X')) {
-            setXYForVoice('X');
+            setXYForVoice('x');
             resetTranscript();
         } else if (transcript.includes('Y')) {
-            setXYForVoice('Y');
+            setXYForVoice('y');
             resetTranscript();
         }
     }, [transcript]);
@@ -162,15 +162,15 @@ const EllipseCanvas: React.FC = () => {
                     setEllipses((prev) => prev.map(ellipse => {
                         if (ellipse.idx === showGuides) {
                             if (volumes[volumes.length - 5] < volumeThreshold) {
-                                if (xyForVoice === 'X') {
+                                if (xyForVoice === 'x') {
                                     return { ...ellipse, x: ellipse.x - 5 };
-                                } else if (xyForVoice === 'Y') {
+                                } else if (xyForVoice === 'y') {
                                     return { ...ellipse, y: ellipse.y - 5 };
                                 }
                             } else {
-                                if (xyForVoice === 'X') {
+                                if (xyForVoice === 'x') {
                                     return { ...ellipse, x: ellipse.x + 5 };
-                                } else if (xyForVoice === 'Y') {
+                                } else if (xyForVoice === 'y') {
                                     return { ...ellipse, y: ellipse.y + 5 };
                                 }
                             }
@@ -182,9 +182,9 @@ const EllipseCanvas: React.FC = () => {
                     setEllipses((prev) => prev.map(ellipse => {
                         if (ellipse.idx === showGuides) {
                             if (volumes[volumes.length - 5] < volumeThreshold) {
-                                return { ...ellipse, rotation: ellipse.rotation - 5 };
+                                return { ...ellipse, rotation: ellipse.rotation - 2 };
                             } else {
-                                return { ...ellipse, rotation: ellipse.rotation + 5 };
+                                return { ...ellipse, rotation: ellipse.rotation + 2 };
                             }
                         }
                         return ellipse;
@@ -194,15 +194,15 @@ const EllipseCanvas: React.FC = () => {
                     setEllipses((prev) => prev.map(ellipse => {
                         if (ellipse.idx === showGuides) {
                             if (volumes[volumes.length - 5] < volumeThreshold) {
-                                if (xyForVoice === 'X') {
+                                if (xyForVoice === 'x') {
                                     return { ...ellipse, radiusX: ellipse.radiusX - 5 };
-                                } else if (xyForVoice === 'Y') {
+                                } else if (xyForVoice === 'y') {
                                     return { ...ellipse, radiusY: ellipse.radiusY - 5 };
                                 }
                             } else {
-                                if (xyForVoice === 'X') {
+                                if (xyForVoice === 'x') {
                                     return { ...ellipse, radiusX: ellipse.radiusX + 5 };
-                                } else if (xyForVoice === 'Y') {
+                                } else if (xyForVoice === 'y') {
                                     return { ...ellipse, radiusY: ellipse.radiusY + 5 };
                                 }
                             }
@@ -220,9 +220,9 @@ const EllipseCanvas: React.FC = () => {
     }, [volume]);
 
     return (
-        <div className="container" style={{ display: 'flex', flexDirection: 'row' }}>
-            <div onKeyDown={handleOnKeyDown} tabIndex={0}>
-                <Stage width={window.innerWidth * 0.6} height={window.innerHeight * 0.6} onDblClick={handleStageClick}>
+        <div className="container" style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+            <div onKeyDown={handleOnKeyDown} tabIndex={0} style={{ flex: '1', border: '1px splid #ddd', borderRadius: '8px', padding: '10px' }}>
+                <Stage width={window.innerWidth * 0.6} height={window.innerHeight * 0.9} onDblClick={handleStageClick}>
                     <Layer>
                         {ellipses.map((ellipse, _) => (
                             removedEllipses.find(removedEllipse => removedEllipse.idx === ellipse.idx) == null &&
@@ -238,35 +238,63 @@ const EllipseCanvas: React.FC = () => {
                     </Layer>
                 </Stage>
             </div>
-            <div>
-                <p>1. 楕円を追加: <small>ダブルクリック, +キー, Aと発音</small></p>
-                <p>2. 選択: <small>クリック, 0-9キー, 0-9と発音 </small></p>
-                {
-                    ellipses.map((ellipse, _) => (
-                        removedEllipses.find(removedEllipse => removedEllipse.idx === ellipse.idx) == null &&
-                            showGuides === ellipse.idx ?
-                            <small><b>{ellipse.idx}, </b></small> : <small>{ellipse.idx}, </small>
-                    ))
-                }
-                <p>3. 移動: <small>マウスドラッグ, msrと矢印キー, msrxyと音量 </small></p>
-                {
-                    ['move', 'scale', 'rotate'].map((mode, _) => (
-                        keyMode === mode ?
-                            <small><b>{mode} : {mode[0]}, </b></small> : <small>{mode} : {mode[0]}, </small>
-                    ))
-                }
-                <small> XY (声操作用): {xyForVoice} </small>
-                <p>4. Voice情報</p>
+            <div className='control' style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                <p style={{ alignSelf: 'flex-start' }}>1. 楕円を追加: <small>ダブルクリック, +キー, Aと発音</small></p>
+                <p style={{ alignSelf: 'flex-start' }}>2. 選択: <small>クリック, 0-9キー, 0-9と発音 </small></p>
+                <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', alignSelf: 'center' }}>
+                    {
+                        ellipses.map((ellipse, _) => (
+                            removedEllipses.find(removedEllipse => removedEllipse.idx === ellipse.idx) == null &&
+                            (showGuides === ellipse.idx ?
+                                <small><b>{ellipse.idx}</b></small> : <small>{ellipse.idx}</small>)
+                        ))
+                    }
+                </div>
+                <p style={{ alignSelf: 'flex-start' }}>3. 移動: <small>マウスドラッグ, msrと矢印キー, msrxyと音量 </small></p>
+                <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', alignSelf: 'center' }}>
+                    {
+                        ['move', 'scale', 'rotate'].map((mode, _) => (
+                            keyMode === mode ?
+                                <small><b>{mode} : {mode[0]}</b></small> : <small>{mode} : {mode[0]}</small>
+                        ))
+                    }
+                    <small> xy (for voice): {xyForVoice} </small>
+                </div>
+                <p style={{ alignSelf: 'flex-start' }}>4. Voice情報</p>
                 <div>
-                    <button onClick={() => { SpeechRecognition.startListening({ continuous: true }); startRecording(); }}>Use Voice</button>
-                    <button onClick={() => { SpeechRecognition.stopListening(); stopRecording(); }}>Stop Voice</button>
-                    <label><small>検知音量:</small></label>
-                    <input type="number" value={volumeMin} onChange={(e) => setVolumeMin(parseInt(e.target.value))} />
-                    <label><small>+-閾値:</small> </label>
-                    <input type="number" value={volumeThreshold} onChange={(e) => setVolumeThreshold(parseInt(e.target.value))} />
-                    <div><small>長さ5, 大きさ{volumeMin}以上の音声を使用. 大きさ{volumeThreshold}以上で+, {volumeThreshold}未満で-</small></div>
-                    <div><small>Your Message: {transcript}</small></div>
-                    <div><small>Your Volume: {volume > volumeMin ? volume : "None"} Your Voice Length: {volumes.length}</small></div>
+                    <div style={{ display: 'flex', flexDirection: 'row', gap: '20px', alignSelf: 'center' }}>
+                        <button
+                            onClick={() => { SpeechRecognition.startListening({ continuous: true }); startRecording(); }}
+                            style={{ borderRadius: '50%', textAlign: 'center' }}>
+                            Start
+                        </button>
+                        <button
+                            onClick={() => { SpeechRecognition.stopListening(); stopRecording(); }}
+                            style={{ borderRadius: '50%', textAlign: 'center' }}>
+                            Stop
+                        </button>
+                        <label><small>検知音量:</small></label>
+                        <input
+                            type="number"
+                            value={volumeMin}
+                            onChange={(e) => setVolumeMin(parseInt(e.target.value))}
+                            style={{ width: '50px' }} />
+                        <label><small>+-閾値:</small> </label>
+                        <input
+                            type="number"
+                            value={volumeThreshold}
+                            onChange={(e) => setVolumeThreshold(parseInt(e.target.value))}
+                            style={{ width: '50px' }} />
+                    </div>
+                    <div>
+                        <small>長さ5, 大きさ{volumeMin}以上の音声を使用. 大きさ{volumeThreshold}以上で+, {volumeThreshold}未満で-
+                            <ul style={{ textAlign: "left" }}>
+                                <li>Your Message: {transcript}</li>
+                                <li>Your Volume: {volume}</li>
+                                <li>Your Voice Length: {volumes.length}</li>
+                            </ul>
+                        </small>
+                    </div>
                 </div>
             </div>
         </div >
